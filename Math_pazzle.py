c4c69d486e36e5727b12c2503afa5c3dc1e43b2b
Taras Zelinskyi <taras.sv89@gmail.com>
2022-11-10 14:20:34 +0200

    third

--- Math_pazzle.py
+++ Math_pazzle.py
@@ -1,15 +1,9 @@
-import numpy
-
-
 def candidate_pairs():
     candidates_pairs = []
-    for number in range(1, 100):
-        for number_2 in range(1, 100):
+    for number in range(2, 100):
+        for number_2 in range(2, 100):
             candidates_pairs.append([number, number_2])
     return candidates_pairs
-
-
-primary_pairs = candidate_pairs()
 
 
 def generate_sums(pairs):
@@ -43,6 +37,10 @@ def generate_products(pairs):
     return prod_dict
 
 
+primary_pairs = candidate_pairs()
+# print(len(primary_pairs))
+
+
 def delete_product_single_pairs(primary_pairs):
     new_pairs = []
     product_pairs = [value[0] for key, value in generate_products(primary_pairs).items() if len(value) == 1]
@@ -52,52 +50,90 @@ def delete_product_single_pairs(primary_pairs):
     return new_pairs
 
 
-def delete_sum_single_pairs(secondary_pairs):
+secondary_pairs = delete_product_single_pairs(primary_pairs)
+# print(secondary_pairs)
+# print(len(secondary_pairs))
+
+
+def check_to_receive_product_need_at_least_two_pairs(secondary_pairs):
+    sums = generate_sums(secondary_pairs)
     new_pairs = []
-    for value in generate_sums(secondary_pairs).values():
-        if len(value) != 1:
-            for pair in value:
+    for key, value in sums.items():
+        for pair in value:
+            prod = pair[0] * pair[1]
+            divider_count = 0
+            for divider in range(2, prod):
+                if prod % divider == 0:
+                    divider_count += 1
+            if divider_count >= 4:
                 new_pairs.append(pair)
 
     return new_pairs
 
 
-def leave_product_single_pairs(third_pairs):
-    new_pairs = []
-    product_pairs = generate_products(third_pairs).values()
-    for pair in product_pairs:
-        if len(pair) == 1:
-            new_pairs.append(pair[0])
-    return new_pairs
-
-
-def leave_sum_single_pairs(fourth_pairs):
-    new_pairs = []
-    product_pairs = generate_sums(fourth_pairs).values()
-    for pair in product_pairs:
-        if len(pair) == 1:
-            new_pairs.append(pair[0])
-    return new_pairs
-
-
-secondary_pairs = delete_product_single_pairs(primary_pairs)
-third_pairs = delete_sum_single_pairs(secondary_pairs)
-fourth_pairs = leave_product_single_pairs(third_pairs)
-final_pair = leave_sum_single_pairs(fourth_pairs)
-
-print(len(secondary_pairs))
+third_pairs = check_to_receive_product_need_at_least_two_pairs(secondary_pairs)
+print(third_pairs)
 print(len(third_pairs))
-print(len(fourth_pairs))
-print(len(final_pair))
 
-# prod = generate_products(final_pair)
-# sums = generate_sums(final_pair)
-
-
-# for key, value in prod.items():
-#         print(f"{key}: {value}")
-#
-# print("=============")
-# for key, value in sums.items():
+generated_prod = generate_products(third_pairs)
+# for key, value in generated_prod.items():
 #     print(f"{key}: {value}")
+
+
+def third_step(pairs, first_pairs):
+    prod_dict = generate_products(pairs)
+    new_pairs = []
+    sums_dict = generate_sums(first_pairs)
+    for key, value in prod_dict.items():
+        pair_count = 0
+        if pair_count == 0:
+            for pair in value:
+                sums = pair[0] + pair[1]
+                additions = sums_dict[sums]
+                for pr in additions:
+                    pr = pr[0] * pr[1]
+                    divider_count = 0
+                    for divider in range(2, pr):
+                        if pr % divider == 0:
+                            divider_count += 1
+                    if divider_count >= 4:
+                        new_pairs.append(tuple(pair))
+                        pair_count += 1
+                        break
+                    break
+                break
+    return set(new_pairs)
+
+
+fourth_pairs = third_step(third_pairs, primary_pairs)
+print(fourth_pairs)
+print(len(fourth_pairs))
+
+
+# third_pairs = check_to_receive_product_need_at_least_two_pairs(secondary_pairs)
+# print(len(third_pairs))
+#
+# def check_to_receive_sum_need_one_pair(third_pairs):
+#     prod = generate_products(third_pairs)
+#     second = generate_sums(third_pairs)
+#     new_pairs = []
+#     for key, value in prod.items():
+#         for pair in value:
+#             sum_ = pair[0] + pair[1]
+#             if sum_ in second:
+#                 new_pairs.append(pair)
+#     return new_pairs
+#
+#
+# def leave_sum_single_pairs(fourth_pairs):
+#     new_pairs = []
+#     product_pairs = generate_sums(fourth_pairs).values()
+#     for pair in product_pairs:
+#         if len(pair) == 1:
+#             new_pairs.append(pair[0])
+#     return new_pairs
+
+
+
+
 
